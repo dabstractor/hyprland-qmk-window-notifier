803b12aa4b4178b7d08da5fb8c9264298b0634a5
Dustin Schultz <dustindschultz@gmail.com>
2025-03-04 19:02:56 -0500

    gnome start, needs javascript shell extension

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -1,8 +1,6 @@
 mod gnome;
 mod hyprland;
 mod macos;
-mod windows;
-mod x11;
 
 use std::error::Error;
 
@@ -20,22 +18,12 @@ pub fn create_monitor(verbose: bool) -> Result<Box<dyn WindowMonitor>, Box<dyn E
         return Ok(Box::new(macos::MacOSMonitor::new(verbose)));
     }
 
-    #[cfg(target_os = "windows")]
-    {
-        return Ok(Box::new(windows::WindowsMonitor::new(verbose)));
-    }
-
     #[cfg(all(target_os = "linux", feature = "hyprland"))]
     {
         // For Linux, we need to detect the specific desktop environment
         if hyprland::is_hyprland_running() {
             return Ok(Box::new(hyprland::HyprlandMonitor::new(verbose)));
         }
-    }
-
-    #[cfg(all(target_os = "linux", feature = "x11"))]
-    {
-        return Ok(Box::new(x11::X11Monitor::new(verbose)));
     }
 
     #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
